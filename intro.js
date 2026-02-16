69588881d5595a1ac023e05477b8d5c93af6fa8e
DAVGAN513 <A01713432@tec.mx>
2026-02-15 23:28:10 -0600

    intrducción a las aplicaciones web y ciclos de vida de SI

---
+++ intro.js
@@ -0,0 +1,14 @@
+//consola (log,info,warn,error,assert)
+console.log("hola!!");
+console.info("informacion");
+console.warn("vertencia");
+console.error("error");
+//Compara valor y tipo de dato
+console.assert(1 === true);
+//variables constantes
+//Forma antigua de declarar variables, NO SE RECOMIENDA
+var videojuego_1 = "Minecraft";
+//Forma correcta de declarar variables
+let videojuego_2 = "Halo";
+
+const precio = 55;
